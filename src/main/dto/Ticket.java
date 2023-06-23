44e659ce5ee450ccc586cc98ce1076861d083cd7
Eliecer Aguilar <eliecer.aguilar1@utp.ac.pa>
2023-06-23 18:39:02 -0500

    ticket data structure

---
+++ src/main/dto/Ticket.java
@@ -0,0 +1,79 @@
+package main.dto;
+
+import java.util.UUID;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Ticket implements Serializable{
+    private String name;
+    private String lastName;
+    private String id;
+    private String email;
+    private String degree;
+    private String ticketID = (UUID.randomUUID()).toString();
+
+    public Ticket() {
+    }
+
+    public Ticket(String name, String lastName, String id, String email, String degree) {
+        this.name = name;
+        this.lastName = lastName;
+        this.id = id;
+        this.email = email;
+        this.degree = degree;
+    }
+    public String getTicketID(){
+        return this.ticketID;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDegree() {
+        return this.degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "name: '" + getName() + "'" + "\n" +
+                        "lastName: '" + getLastName() + "'" +"\n" +
+                        "id: '" + getId() + "'" +"\n" +
+                        "email: '" + getEmail() + "'" +"\n" +
+                        "degree: '" + getDegree() + "'" + "\n";
+    }
+
+}
